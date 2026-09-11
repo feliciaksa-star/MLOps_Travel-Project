857089bb60464cfcfd364d663a2d759e25fc7024
feliciaksa-star <feliciaksa@gmail.com>
2026-09-11 16:52:12 +0200

    update tourism_project/model_building/prep.py

--- tourism_project/model_building/prep.py
+++ tourism_project/model_building/prep.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv("tourism_project/data/tourism.csv")
+df = pd.read_csv("tourism_project/tourism.csv")
 df.drop(columns=["CustomerID"], inplace=True)
 
 # NOTE: categorical columns are intentionally left as raw strings.
